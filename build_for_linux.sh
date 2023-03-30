e65a435f75bd17ef72366cc6ce4be2bc3e45b4d1
Brennan Harris <brennan@brennanharris.xyz>
2023-03-29 22:29:06 -0600

    updated chicken_door target

---
+++ build_for_linux.sh
@@ -0,0 +1,11 @@
+brew tap messense/macos-cross-toolchains
+brew install x86_64-unknown-linux-gnu
+
+export CC_X86_64_UNKNOWN_LINUX_GNU=x86_64-unknown-linux-gnu-gcc
+export CXX_X86_64_UNKNOWN_LINUX_GNU=x86_64-unknown-linux-gnu-g++
+export AR_X86_64_UNKNOWN_LINUX_GNU=x86_64-unknown-linux-gnu-ar
+export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER=x86_64-unknown-linux-gnu-gcc
+
+rustup target add x86_64-unknown-linux-gnu
+
+cargo build --target x86_64-unknown-linux-gnu
